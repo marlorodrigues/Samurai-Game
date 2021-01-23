ad0dc332f7efc7fcf57c1e348f420d1abf441491
marlorodrigues <49593773+marlorodrigues@users.noreply.github.com>
2021-01-22 21:45:15 -0300

    Commit Initial

---
+++ Assets/Scripts/Flight Enemy/FlightEnemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlightEnemy : MonoBehaviour
+{
+    public float Speed;
+    public Rigidbody2D rigidBody;
+    public Animator animator;
+    public Transform player;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed;
+    public float jumpForce;
+    private bool isJumping;
+    private bool isAttacking;
+    private int attackCombo = 0;
+    private int auxFlag = 0;
+
+    public Rigidbody2D rigidBody;
+    public Animator animator;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Jump();
+        Attack();
+    }
+
+    private void FixedUpdate() {
+        float direction = Input.GetAxis("Horizontal");
+
+        rigidBody.velocity = new Vector2(speed * direction, rigidBody.velocity.y);
+
+        if(direction == 0){
+            if(auxFlag > 60)
+                auxFlag = 0;
+            else
+                auxFlag++;
+ 
+            changeAnimation(0);
+        }
+        else if(direction > 0){
+            transform.eulerAngles = new Vector2(0, 0);
+            changeAnimation(1);
+        }
+        else{
+            if(!isAttacking){
+                transform.eulerAngles = new Vector2(0, -180);
+                changeAnimation(1);
+            }
+        }
+    }
+
+    private void Jump(){
+        if(Input.GetButtonDown("Jump")){
+            if(!isJumping){
+                rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                changeAnimation(2);
+                isJumping = true;
+            }
+        }
+    }
+
+    private void Attack(){
+        if(Input.GetButtonDown("Fire1")){
+            if (!isAttacking)
+            {
+                if(attackCombo < 2){
+                    AttackLight();
+                    attackCombo++; 
+                }
+                else{
+                    AttackHeavy();
+                    attackCombo = 0;  
+                }
+            }
+        }
+    }
+
+    private void AttackLight(){
+        changeAnimation(3);
+        isAttacking = true;
+        StartCoroutine(onAttacking());          //wait for the time of animation
+    }
+
+    private void AttackHeavy(){
+        changeAnimation(4);
+        isAttacking = true;
+        StartCoroutine(onAttacking());             //wait for the time of animation
+    }
+
+    private void changeAnimation(int number){
+        if(!isJumping && !isAttacking)
+            animator.SetInteger("transition", number);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if(collision.gameObject.layer == 8){
+            isJumping = false;
+        }
+    }
+
+    //Wait for the time of animation
+    IEnumerator onAttacking(){
+        yield return new WaitForSeconds(0.6f);
+        isAttacking = false;
+    }
+}
